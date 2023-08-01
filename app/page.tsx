9fa738c6fbcffac3900b4df866d65a5ba6399e11
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-08-01 23:10:24 +0900

    Fetch data from sanity

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
-export default function IndexPage() {
+import { client } from "./lib/sanity";
+
+async function getData() {
+  const query = `*[_type == "post"]`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function IndexPage() {
+  const data = await getData();
 
   return (
     <h1>Hi from the index page</h1>
